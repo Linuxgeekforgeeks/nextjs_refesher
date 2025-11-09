e37637d3aff671f99502629f6fa94f9eff0a041a
linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2025-11-09 01:51:25 -0800

    Image optimazation

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image"
 
 
 function LoginPage() {
   return (
     <div>
       This is the Login Page.
+      <div>
+
+      <Image alt="Image" src={"/next.svg"} height={300} width={130}/>
+      </div>
     </div>
   )
 }
